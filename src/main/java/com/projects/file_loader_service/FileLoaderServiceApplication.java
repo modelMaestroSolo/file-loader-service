83203b0252c7b98835def39deb4202d2bca63ca5
Solomon Yeboah <modelmaestrosolo@gmail.com>
2026-06-25 06:37:30 +0000

    setup project
     - boostrap spring application.

---
+++ src/main/java/com/projects/file_loader_service/FileLoaderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.projects.file_loader_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileLoaderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileLoaderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/file_loader_service/FileLoaderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.file_loader_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileLoaderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
