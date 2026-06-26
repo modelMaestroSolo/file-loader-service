ea6d4bdb8699f16b988c3d3792d5c618671cc6d7
Solomon Yeboah <modelmaestrosolo@gmail.com>
2026-06-26 13:26:51 +0000

    Implement folder polling scheduler

--- src/main/java/com/projects/file_loader_service/FileLoaderServiceApplication.java
+++ src/main/java/com/projects/file_loader_service/FileLoaderServiceApplication.java
@@ -6,22 +6,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.nio.file.Path;
 
+@EnableScheduling
 @SpringBootApplication
 public class FileLoaderServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FileLoaderServiceApplication.class, args);
 	}
-
-    @Bean
-    public CommandLineRunner runFileLoader(
-            FileProcessorService fileProcessorService
-            ){
-        Path file = Path.of("C:\\Users\\yebso\\Documents\\PAIC project files\\cdr.log.20230818_10_reducted");
-        return args -> fileProcessorService.process(file);
-    }
 
 }
